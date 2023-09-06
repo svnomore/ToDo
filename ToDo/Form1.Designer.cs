dd042fda7a005d5066b86d6481d8851e5f694fea
svnomore <svnomore@gmail.com>
2023-09-06 14:49:33 +0300

    Main Files

---
+++ ToDo/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace ToDo
+{
+    partial class ToDo
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ToDo));
+            button1 = new Button();
+            textBox1 = new TextBox();
+            button2 = new Button();
+            listBox1 = new ListBox();
+            comboBox1 = new ComboBox();
+            textBox2 = new TextBox();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(12, 44);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 25);
+            button1.TabIndex = 0;
+            button1.Text = "Add";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += AddTask;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(12, 13);
+            textBox1.MaxLength = 90;
+            textBox1.Name = "textBox1";
+            textBox1.PlaceholderText = "Add your task...";
+            textBox1.Size = new Size(767, 22);
+            textBox1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(93, 44);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 25);
+            button2.TabIndex = 4;
+            button2.Text = "Remove";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += RemoveTask;
+            // 
+            // listBox1
+            // 
+            listBox1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            listBox1.Font = new Font("Arial", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            listBox1.ForeColor = SystemColors.MenuText;
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 18;
+            listBox1.Location = new Point(12, 75);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(767, 382);
+            listBox1.TabIndex = 15;
+            // 
+            // comboBox1
+            // 
+            comboBox1.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBox1.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+            comboBox1.FormattingEnabled = true;
+            comboBox1.ImeMode = ImeMode.NoControl;
+            comboBox1.Items.AddRange(new object[] { "8", "10", "12", "14", "16", "18", "20", "22" });
+            comboBox1.Location = new Point(727, 43);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(52, 25);
+            comboBox1.TabIndex = 7;
+            comboBox1.SelectedIndexChanged += SettingsData;
+            // 
+            // textBox2
+            // 
+            textBox2.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            textBox2.BackColor = SystemColors.Control;
+            textBox2.BorderStyle = BorderStyle.None;
+            textBox2.Location = new Point(621, 48);
+            textBox2.Name = "textBox2";
+            textBox2.RightToLeft = RightToLeft.Yes;
+            textBox2.Size = new Size(100, 15);
+            textBox2.TabIndex = 16;
+            textBox2.Text = "Font Size";
+            // 
+            // ToDo
+            // 
+            AutoScaleDimensions = new SizeF(7F, 16F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ButtonFace;
+            ClientSize = new Size(791, 480);
+            Controls.Add(textBox2);
+            Controls.Add(comboBox1);
+            Controls.Add(listBox1);
+            Controls.Add(button2);
+            Controls.Add(textBox1);
+            Controls.Add(button1);
+            Font = new Font("Arial", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "ToDo";
+            SizeGripStyle = SizeGripStyle.Show;
+            Text = "ToDo v0.1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private TextBox textBox1;
+        private Button button2;
+        private ListBox listBox1;
+        private ComboBox comboBox1;
+        public TextBox textBox2;
+    }
+}
---
+++ ToDo/Form1.cs
@@ -0,0 +1,123 @@
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Drawing;
+using System.IO;
+
+namespace ToDo
+{
+    public partial class ToDo : Form
+    {
+        private static string documentsPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "ToDo");
+        private string tasksFilePath = Path.Combine(documentsPath, "tasks.txt");
+        private string settingsFilePath = Path.Combine(documentsPath, "settings.txt");
+
+        public ToDo()
+        {
+            InitializeComponent();
+            LoadData();
+
+            if (!Directory.Exists(documentsPath))
+            {
+                try
+                {
+                    Directory.CreateDirectory(documentsPath);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+            }
+
+            if (!File.Exists(tasksFilePath))
+            {
+                try
+                {
+                    File.WriteAllText(tasksFilePath, "");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+            }
+
+            if (!File.Exists(settingsFilePath))
+            {
+                try
+                {
+                    File.WriteAllText(settingsFilePath, "10");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+            }
+
+            if (comboBox1.SelectedIndex == -1)
+            {
+                comboBox1.SelectedItem = File.ReadAllText(settingsFilePath);
+            }
+        }
+        private void AddTask(object sender, EventArgs e)
+        {
+            if (textBox1.TextLength != 0)
+            {
+                listBox1.Items.Add(textBox1.Text);
+                textBox1.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Write task to add.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            SaveData();
+        }
+
+        private void RemoveTask(object sender, EventArgs e)
+        {
+            try
+            {
+                listBox1.Items.RemoveAt(listBox1.SelectedIndex);
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                MessageBox.Show("Select task to remove.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            SaveData();
+        }
+
+        private void SaveData()
+        {
+            try
+            {
+                File.WriteAllLines(tasksFilePath, listBox1.Items.Cast<string>().ToArray());
+                File.WriteAllText(settingsFilePath, comboBox1.SelectedItem.ToString());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        private void LoadData()
+        {
+            try
+            {
+                listBox1.Items.Clear();
+                listBox1.Items.AddRange(File.ReadAllLines(tasksFilePath));
+                listBox1.Font = new Font("Arial", float.Parse(File.ReadAllText(settingsFilePath)));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        private void SettingsData(object sender, EventArgs e)
+        {
+            File.WriteAllText(settingsFilePath, comboBox1.SelectedItem.ToString());
+            listBox1.Font = new Font("Arial", float.Parse(File.ReadAllText(settingsFilePath)));
+        }
+    }
+}
